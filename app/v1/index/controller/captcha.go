892fe195c36211ce8aaddd616df597c1aa592bcc
Tuuz <2539@tuuz.cn>
2025-12-05 03:19:28 +0800

    更新 AossGoSdk 到 v1.19.0 并修改验证码 Token 为项目配置

--- app/v1/index/controller/captcha.go
+++ app/v1/index/controller/captcha.go
@@ -4,16 +4,19 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/tobycroft/AossGoSdk"
 	"github.com/tobycroft/Calc"
+	"main.go/common/BaseController"
+	"main.go/config/app_conf"
 	"main.go/tuuz/RET"
 )
 
 func CaptchaController(route *gin.RouterGroup) {
+	route.Use(BaseController.CommonController())
 	route.Any("/get", captcha_get)
 }
 
 func captcha_get(c *gin.Context) {
 	var captcha AossGoSdk.Captcha
-	captcha.Token = "ps"
+	captcha.Token = app_conf.Project
 	oid := Calc.GenerateOrderId()
 	img, err := captcha.Math(oid)
 	if err != nil {
