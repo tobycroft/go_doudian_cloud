d2851ce30f640ed5b1a973aa507395ce02898836
Tuuz <2539@tuuz.cn>
2025-12-16 23:28:09 +0800

    更新用户模型和控制器，添加通过 ID 查询用户功能并调整路由配置

--- app/v1/user/controller/info.go
+++ app/v1/user/controller/info.go
@@ -2,15 +2,22 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
+	"main.go/app/v1/user/model/UserModel"
 	"main.go/common/BaseController"
+	"main.go/tuuz/RET"
 )
 
 func InfoController(route *gin.RouterGroup) {
 	route.Use(BaseController.CommonController())
 	route.Use(BaseController.LoginedController())
-	route.Any("/info", info_get)
+	route.Any("get", info_get)
 }
 
 func info_get(c *gin.Context) {
-
+	uid := c.GetHeader("uid")
+	if user, err := UserModel.Api_findById(uid); err != nil {
+		RET.Fail(c, 500, nil, nil)
+	} else {
+		RET.Success(c, 0, user, nil)
+	}
 }
--- app/v1/user/model/UserModel/UserModel.go
+++ app/v1/user/model/UserModel/UserModel.go
@@ -1,6 +1,9 @@
 package UserModel
 
-import "main.go/tuuz"
+import (
+	"github.com/tobycroft/gorose-pro"
+	"main.go/tuuz"
+)
 
 const Table = "doudian_user"
 
@@ -14,8 +17,14 @@ func Api_insert(username, mail, password any) (id int64, err error) {
 	return db.InsertGetId()
 }
 
-func Api_findByEmail(mail any) (data map[string]any, err error) {
+func Api_findByEmail(mail any) (data gorose.Data, err error) {
 	db := tuuz.Db().Table(Table)
 	db.Where("mail", "=", mail)
+	return db.Find()
+}
+
+func Api_findById(id any) (data gorose.Data, err error) {
+	db := tuuz.Db().Table(Table)
+	db.Where("id", "=", id)
 	return db.Find()
 }
